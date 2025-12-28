1ab1f9d37c172fda82da96533d20ae39319cdf41
Dipin Karunakaran <114206930+dipin736@users.noreply.github.com>
2025-12-28 22:28:37 +0530

    files changed for deployment

--- campaign-dashboard/vite.config.ts
+++ campaign-dashboard/vite.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/vite"
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '', 'VITE_');
-    return {
-        plugins: [react(),tailwindcss()],
-        base: env.VITE_BASE_URL,
-        server: {
-            port: 44352
-        },
-        optimizeDeps: {
-            include: [''], 
-          },
-    };
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 44352,
+  },
 });
